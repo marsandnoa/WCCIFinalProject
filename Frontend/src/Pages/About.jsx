dcb5fe0790a449cdba1037a6cbe7e14112a03c4b
unknown <marsandnoa@gmail.com>
2023-09-26 19:57:44 -0400

    copying from WCCI

--- Frontend/src/Pages/About.jsx
+++ Frontend/src/Pages/About.jsx
@@ -4,32 +4,11 @@ import logo from "../App Components/anglerfish.png";
 const About = () => {
   const STUDENT_DATA = [
     {
-      id: "s1",
-      name: "Brian Mgbokwere",
-      image: "../src/About Components/Student Images/Brian.jpg",
-      biography:
-        "I am a graduate from Northern Illinois University, where I earned my Bachelor of Science in Health Sciences. I have always had an interest in software development.",
-    },
-    {
       id: "s2",
       name: "Noah Marsteller",
-      image: "../src/About Components/Student Images/noah.jpg",
+      image: "",
       biography:
         "I am a Cincinnati native, and I work at Amazon Air in Wilmington",
-    },
-    {
-      id: "s3",
-      name: "Steven Lin",
-      image: "../src/About Components/Student Images/Steven.jpg",
-      biography:
-        "I recently graduated from Pennsylvania State University with Information Science and Technology degree. IST is a very awesome major, we have cybersecurity class, UI/UX design class and a lots of programming class. This is how I start step into the Tech field",
-    },
-    {
-      id: "s4",
-      name: "Yailin Acosta",
-      image: "../src/About Components/Student Images/Yailin.jpg",
-      biography:
-        "I have multiple certifications, in the health and food industry, but I have always had on interested in the tech field. Once I started working at Amazon, I decided to study software development.",
     },
   ];
 
